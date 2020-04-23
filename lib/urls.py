1938b35867e4ba3d5175a8b34056dfbb6c78f80d
longzhenhong <longzhenhong220@126.com>
2020-04-24 00:06:24 +0800

    addBook

--- lib/urls.py
+++ lib/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'lib'
 urlpatterns = [
+        path('addBook/',views.addBook,name='addBook'),
         path('',views.index,name='index'),
-        path('detail/',views.detail,name='detail')
+        path('detail/',views.detail,name='detail'),
         ]
